d6a1665ff0ee837f83cc696f663a5969923b61e2
Paul Backus <snarwin@gmail.com>
2021-02-14 12:00:30 -0500

    Use dmd.frontend API to drive the parser

--- src/main.d
+++ src/main.d
@@ -2,6 +2,7 @@ module main;
 
 import std.meta: AliasSeq;
 
+import dmd.frontend: initDMD, parseModule;
 import dmd.declaration: AliasDeclaration, VarDeclaration;
 import dmd.func: FuncDeclaration;
 import dmd.denum: EnumDeclaration, EnumMember;
@@ -28,12 +29,15 @@ int main(string[] args)
 
 void tryMain(string[] args)
 {
+	import std.exception: enforce;
+
 	checkUsage(args);
 	string path = args[1];
-	initializeDMD();
-	Module parsedMod = parseModule(path);
+	initDMD();
+	auto parseResult = parseModule(path);
+	enforce(parseResult.diagnostics.errors == 0, "Module failed to parse");
 	scope v = new DeclarationVisitor();
-	parsedMod.accept(v);
+	parseResult.module_.accept(v);
 }
 
 void checkUsage(string[] args)
@@ -43,43 +47,6 @@ void checkUsage(string[] args)
 
 	enforce(args.length >= 2, "Must pass a source file as an argument");
 	enforce(args[1].endsWith(".d", ".di"), "Source file must end in .d or .di");
-}
-
-void initializeDMD()
-{
-	import dmd.mtype: Type;
-	import dmd.id: Id;
-	import dmd.dmodule: Module;
-	//import dmd.target: target;
-	//import dmd.expression: Expression;
-	//import dmd.objc: Objc;
-	//import dmd.filecache: FileCache;
-	//import dmd.globals: global;
-
-	Type._init();
-	Id.initialize();
-	Module._init();
-	// Since we're just parsing, these shouldn't be necessary...
-	//target._init(global.params);
-	//Expression._init();
-	//Objc._init();
-	//FileCache._init();
-}
-
-Module parseModule(string path)
-{
-	import std.path: baseName, stripExtension;
-	import std.exception: enforce;
-
-	import dmd.identifier: Identifier;
-	import dmd.globals: Loc;
-
-	string modname = path.baseName.stripExtension;
-	Identifier id = Identifier.idPool(modname);
-	auto mod = new Module(path, id, false, false);
-	bool success = mod.read(Loc.initial);
-	enforce(success, "Failed to read module");
-	return mod.parse();
 }
 
 void writeTag(Dsymbol sym)
