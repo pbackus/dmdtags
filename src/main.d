c52025a1879b25210de1eaedf9115d9c4aa4943d
Paul Backus <snarwin@gmail.com>
2021-08-08 18:14:48 -0400

    Sort tags before writing output
    Use of void* is necessary to ensure that no extern(C++) symbol depends
    in any way (including via name-mangling) on a D slice type, since D
    slices have no C++ equivalent.

--- src/main.d
+++ src/main.d
@@ -29,18 +29,33 @@ int main(string[] args)
 
 void tryMain(string[] args)
 {
-	import std.exception: enforce;
+	import std.array: appender;
+	import std.algorithm: sort, each;
+	import std.stdio: writeln;
 
 	checkUsage(args);
 	string[] paths = args[1 .. $];
 
 	initDMD();
-	scope tagger = new SymbolTagger();
+	auto tags = appender!(const(char)[][]);
+
+	static extern(C++)
+	void sinkFn(const(char)* ptr, size_t length, void* context)
+	{
+		import std.range: put;
+
+		put(*cast(typeof(tags)*) context, ptr[0 .. length]);
+	}
+
+	scope tagger = new SymbolTagger(&sinkFn, &tags);
 
 	foreach (path; paths) {
 		auto parseResult = parseModule(path);
 		parseResult.module_.accept(tagger);
 	}
+
+	sort(tags[]);
+	tags[].each!writeln;
 }
 
 void checkUsage(string[] args)
@@ -52,24 +67,28 @@ void checkUsage(string[] args)
 	enforce(args[1].endsWith(".d", ".di"), "Source file must end in .d or .di");
 }
 
-void writeTag(Dsymbol sym)
+alias TagSink = extern(C++) void function(const(char)*, size_t, void*);
+
+void writeTag(TagSink sink, void* context, Dsymbol sym)
 {
-	import std.stdio: writefln;
+	import std.format;
 	import dmd.root.string: toDString;
 
 	if (!sym.loc.isValid) return;
 	const(char)[] filename = sym.loc.filename.toDString;
 	if (!filename) return;
-	writefln("%s\t%s\t%s", sym.ident.toString, filename, sym.loc.linnum);
+	string tag = format("%s\t%s\t%s", sym.ident.toString, filename, sym.loc.linnum);
+	sink(tag.ptr, tag.length, context);
 }
 
-void writeTag(Module m)
+void writeTag(TagSink sink, void* context, Module m)
 {
-	import std.stdio: writefln;
+	import std.format;
 
 	if (!m.srcfile.name) return;
 	size_t line = m.md ? m.md.loc.linnum : 1;
-	writefln("%s\t%s\t%s", m.ident.toString, m.srcfile.toString, line);
+	string tag = format("%s\t%s\t%s", m.ident.toString, m.srcfile.toString, line);
+	sink(tag.ptr, tag.length, context);
 }
 
 alias TaggableSymbols = AliasSeq!(
@@ -90,6 +109,15 @@ extern(C++) class SymbolTagger : SemanticTimeTransitiveVisitor
 {
 	import dmd.dsymbol: ScopeDsymbol;
 
+	private void* context;
+	private TagSink sink;
+
+	extern(D) this(TagSink sink, void* context)
+	{
+		this.sink = sink;
+		this.context = context;
+	}
+
 	alias visit = typeof(super).visit;
 
 	void visitMembers(ScopeDsymbol s)
@@ -105,7 +133,7 @@ extern(C++) class SymbolTagger : SemanticTimeTransitiveVisitor
 	static foreach (Symbol; TaggableSymbols) {
 		override void visit(Symbol sym)
 		{
-			writeTag(sym);
+			writeTag(sink, context, sym);
 			static if (is(Symbol : ScopeDsymbol))
 				visitMembers(sym);
 		}
